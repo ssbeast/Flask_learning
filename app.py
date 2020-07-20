d8db73d3dc13a25cb19da2c24b06747ab91d30e8
ssbeast <ssbeast007@gmail.com>
2020-07-20 07:50:39 +0530

    Todo class added

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app  = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.column(db.Integer, primary_key=True)
+    content = db.column(db.String(200), nullable=False)
+    date_creadted = db.column(db.DateTime, default=datetime.utcnow)
 
 @app.route('/')
 def index():
