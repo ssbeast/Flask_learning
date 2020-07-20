6c63eea7951dfef1a022b7f9869d02a44e3c563d
ssbeast <ssbeast007@gmail.com>
2020-07-20 07:57:55 +0530

    database added

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
-    id = db.column(db.Integer, primary_key=True)
-    content = db.column(db.String(200), nullable=False)
-    date_creadted = db.column(db.DateTime, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
